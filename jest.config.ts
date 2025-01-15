cc795498dad482272d40be368931a83ac05e8d3c
shu.saginoya <shu.saginoya@gmail.com>
2025-01-15 16:41:48 +0900

    Add Jest config

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "jest";
+import nextJest from "next/jest";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  coverageProvider: "v8",
+  testEnvironment: "jest-environment-jsdom",
+  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  moduleDirectories: ["node_modules", "<rootDir>/src"],
+};
+
+export default createJestConfig(config);
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  compiler: (() => {
+    let compilerConfig = {};
+    if (process.env.NODE_ENV === "production") {
+      compilerConfig = {
+        ...compilerConfig,
+        // 本番環境ではReact Testing Libraryで使用するdate-testid属性を削除
+        reactRemoveProperties: { properties: ["^date-testid$"] },
+      };
+    }
+
+    return compilerConfig;
+  })(),
 };
 
 export default nextConfig;
