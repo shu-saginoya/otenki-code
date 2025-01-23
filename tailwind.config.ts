985731a3b2625f581e1a4336305b1dd77fd982b2
shu.saginoya <shu.saginoya@gmail.com>
2025-01-23 21:48:41 +0900

    Update Tailwind config colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,27 +9,46 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: "#10b981",
-        primaryDark: "#065f46",
-        primaryLight: "#a7f3d0",
-        secondary: "#0ea5e9",
-        secondaryDark: "#075985",
-        secondaryLight: "#bae6fd",
-        background: "#e4e4e7",
-        backgroundDark: "#171717",
-        backgroundLight: "#fafafa",
-        success: "#22c55e",
-        successDark: "#166534",
-        successLight: "#bbf7d0",
-        error: "#ef4444",
-        errorDark: "#991b1b",
-        errorLight: "#fecaca",
-        warning: "#f97316",
-        warningDark: "#9a3412",
-        warningLight: "#fed7aa",
-        disabled: "#6b7280",
-        disabledDark: "#1f2937",
-        disabledLight: "#e5e7eb",
+        primary: {
+          light: "#a7f3d0", // emerald-200
+          DEFAULT: "#10b981", // emerald-500
+          dark: "#065f46", // emerald-800
+        },
+        foreground: {
+          light: "#404040", // neutral-700
+          DEFAULT: "#262626", // neutral-800
+          dark: "#171717", // neutral-900
+        },
+        background: {
+          light: "#ffffff", // white
+          DEFAULT: "#f4f4f5", // neutral-100
+          dark: "#e4e4e7", // neutral-200
+        },
+        disabled: {
+          light: "#a3a3a3", // neutral-400
+          DEFAULT: "#737373", // neutral-500
+          dark: "#525252", // neutral-600
+        },
+        link: {
+          light: "#7dd3fc", // sky-300
+          DEFAULT: "#0ea5e9", // sky-500
+          dark: "#0369a1", // sky-700
+        },
+        success: {
+          light: "#86efac", // green-300
+          DEFAULT: "#22c55e", // green-500
+          dark: "#15803d", // green-700
+        },
+        error: {
+          light: "#fca5a5", // red-300
+          DEFAULT: "#ef4444", // red-500
+          dark: "#b91c1c", // red-700
+        },
+        warning: {
+          light: "#fdba74", // orange-300
+          DEFAULT: "#f97316", // orange-500
+          dark: "#c2410c", // orange-700
+        },
       },
     },
   },
