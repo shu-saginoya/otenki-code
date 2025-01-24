9c9da144e4c83f4b7b4201f9b6b2b7dcb2d5e544
shu.saginoya <shu.saginoya@gmail.com>
2025-01-24 16:39:41 +0900

    Update Layouts
    Support for inline elements

--- src/components/layout/Grid/index.tsx
+++ src/components/layout/Grid/index.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 
 export type GridProps = {
   children: ReactNode;
+  inline?: boolean;
   className?: string;
 };
 
@@ -10,11 +11,13 @@ export type GridProps = {
  * レイアウトの調整に利用するフレックスボックスコンテナ
  *
  * @param children 子要素
+ * @param inline インライン要素(真偽値)
  * @param className 追加のクラス名（任意）
  * @returns
  */
-const Grid = ({ children, className }: GridProps) => {
-  return <div className={`grid ${className}`}>{children}</div>;
+const Grid = ({ children, inline = false, className }: GridProps) => {
+  const display = inline ? "inline-grid" : "grid";
+  return <div className={`${display} ${className}`}>{children}</div>;
 };
 
 export default Grid;
