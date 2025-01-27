f4fc8d2b3513b74e961181e127b7becc20d92c22
shu.saginoya <shu.saginoya@gmail.com>
2025-01-27 23:00:47 +0900

    Add atoms/Switch

---
+++ src/components/atoms/Switch/index.tsx
@@ -0,0 +1,36 @@
+import { Switch as HUSwitch } from "@headlessui/react";
+import { FC } from "react";
+
+type SwitchProps = {
+  checked: boolean;
+  onChange: (checked: boolean) => void;
+  disabled?: boolean;
+};
+
+/**
+ * トグルスイッチコンポーネント
+ * @param checked - スイッチの状態
+ * @param onChange - 状態変更時のコールバック
+ * @param disabled - 無効状態
+ */
+const Switch: FC<SwitchProps> = ({ checked, onChange, disabled = false }) => {
+  return (
+    <HUSwitch
+      checked={checked}
+      onChange={onChange}
+      disabled={disabled}
+      aria-label="Toggle switch"
+      className={
+        "group inline-flex h-6 w-11 items-center rounded-full bg-disabled transition-colors disabled:cursor-not-allowed  disabled:opacity-25 data-[checked]:bg-primary"
+      }
+    >
+      <span
+        className={
+          "size-4 translate-x-1 rounded-full bg-white transition-transform group-data-[checked]:translate-x-6"
+        }
+      />
+    </HUSwitch>
+  );
+};
+
+export default Switch;
