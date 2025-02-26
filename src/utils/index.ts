494d7cd67938139bf11451405b6ff426a025b690
shu.saginoya <shu.saginoya@gmail.com>
2025-02-26 12:36:29 +0900

    Update utils/index.ts fetcher

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,19 +1,24 @@
-export const fetcher = async (
+/**
+ * 指定されたリソースからデータを取得し、JSONオブジェクトとして返す
+ *
+ * @template T - 期待される応答データの型
+ * @param {RequestInfo} resource - フェッチしたいリソース
+ * @param {RequestInit} [init] - リクエストに適用したいカスタム設定を含むオプションのオブジェクト
+ * @returns {Promise<T>} JSON形式のレスポンスデータを解決するプロミス
+ * @throws {Error} レスポンスが正常でない場合にエラーをスローする
+ */
+export const fetcher = async <T>(
   resource: RequestInfo,
-  init?: RequestInit,
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-): Promise<any> => {
+  init?: RequestInit
+): Promise<T> => {
   const res = await fetch(resource, init);
 
   if (!res.ok) {
-    // レスポンスが失敗した時に例外を投げる
     const errorRes = await res.json();
-    const error = new Error(
-      errorRes.message ?? "APIリクエスト中にエラーが発生しました",
-    );
-
-    throw error;
+    const errorMessage =
+      errorRes?.message || "APIリクエスト中にエラーが発生しました";
+    throw new Error(errorMessage);
   }
 
-  return res.json();
+  return res.json() as Promise<T>;
 };
