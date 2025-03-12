1e29c35fb2d87dd536b8f327d5cc17550a155b5f
shu.saginoya <shu.saginoya@gmail.com>
2025-03-12 16:01:07 +0900

    Fix atoms/Badge

--- src/components/atoms/Badge/index.tsx
+++ src/components/atoms/Badge/index.tsx
@@ -1,6 +1,5 @@
 import { FC } from "react";
 import clsx from "clsx";
-import Flex from "@/components/layout/Flex";
 import Text from "@/components/atoms/Text";
 
 export type BadgeProps = {
@@ -11,18 +10,19 @@ export type BadgeProps = {
   /**
    * ユーティリティクラス
    */
-  className: string;
+  className?: string;
 };
 
 /**
  * バッジ
  */
 const Badge: FC<BadgeProps> = ({ content, className }) => {
-  const baseStyle = "h-5 min-w-5 items-center justify-center rounded-full px-1";
+  const baseStyle =
+    "h-5 min-w-5 items-center justify-center rounded-full px-1 inline-flex";
   return (
-    <Flex inline className={clsx(baseStyle, className)}>
+    <div className={clsx(baseStyle, className)}>
       <Text className="text-xs text-white">{content}</Text>
-    </Flex>
+    </div>
   );
 };
 
