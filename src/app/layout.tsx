ebe8692fb342d7946e97cefe111dde1715029e2d
shu.saginoya <shu.saginoya@gmail.com>
2025-03-14 23:52:48 +0900

    Update Metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,8 +7,10 @@ import Header from "@/components/organisms/Header";
 import Footer from "@/components/organisms/Footer";
 
 export const metadata: Metadata = {
-  title: "ふくそう日和",
-  description: "検討中",
+  title: "お天気コーデ",
+  description:
+    "気象庁の天気・気温予報をもとに服装の目安をお知らせします。忙しい毎日にご活用ください。",
+  keywords: ["気温", "服装", "お着替え"],
 };
 
 export default function RootLayout({
