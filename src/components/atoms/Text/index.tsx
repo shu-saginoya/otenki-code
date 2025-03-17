e3bb4660227daf00c0e188a401d222e12ac621a5
shu.saginoya <shu.saginoya@gmail.com>
2025-03-17 23:40:38 +0900

    Update components/atoms/Text

--- src/components/atoms/Text/index.tsx
+++ src/components/atoms/Text/index.tsx
@@ -1,22 +1,59 @@
-import { ReactNode, ComponentProps, JSX } from "react";
+import { ReactNode, JSX } from "react";
+import clsx from "clsx";
+import {
+  fontFamilyMap,
+  FontFamily,
+  fontSizeMap,
+  FontSize,
+  fontWeightMap,
+  FontWeight,
+  letterSpacingMap,
+  LetterSpacing,
+} from "@/utils";
 
 export type TextProps = {
   children: ReactNode;
+  as?: "span" | "p" | "label" | "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+  family?: FontFamily;
+  size?: FontSize;
+  weight?: FontWeight;
+  spacing?: LetterSpacing;
   className?: string;
-} & ComponentProps<"span">;
+};
 
 /**
  * Text component
  * インライン要素のテキストを扱うコンポーネント
  * @param children 子要素
+ * @param as - タグの指定(デフォルトspan)
+ * @param family - フォントファミリー
+ * @param size - フォントサイズ
+ * @param weight - フォントウェイト
+ * @param spacing - 文字間隔
  * @param className 追加のクラス名（任意）
  * @returns
  */
-const Text = ({ children, className, ...props }: TextProps): JSX.Element => {
+const Text = ({
+  children,
+  as: Component = "span",
+  family,
+  size,
+  weight,
+  spacing,
+  className,
+}: TextProps): JSX.Element => {
   return (
-    <span {...props} className={className}>
+    <Component
+      className={clsx(
+        family !== undefined && fontFamilyMap[family],
+        size !== undefined && fontSizeMap[size],
+        weight !== undefined && fontWeightMap[weight],
+        spacing !== undefined && letterSpacingMap[spacing],
+        className
+      )}
+    >
       {children}
-    </span>
+    </Component>
   );
 };
 
--- src/utils/tailwindcss/typography.ts
+++ src/utils/tailwindcss/typography.ts
@@ -1,9 +1,45 @@
-// 文字スタイル
-export const textStyleMap = {
-  title: "text-xl font-bold",
-  subtitle: "text-lg font-bold",
-  base: "text-base",
-  caption: "text-sm",
+export const fontFamilyMap = {
+  sans: "font-sans",
+  serif: "font-serif",
+  mono: "font-mono",
 } as const;
 
-export type TextStyle = keyof typeof textStyleMap;
+export type FontFamily = keyof typeof fontFamilyMap;
+
+export const fontSizeMap = {
+  xs: "text-xs",
+  sm: "text-sm",
+  base: "text-base",
+  lg: "text-lg",
+  xl: "text-xl",
+  "2xl": "text-2xl",
+  "3xl": "text-3xl",
+  "4xl": "text-4xl",
+  "5xl": "text-5xl",
+  "6xl": "text-6xl",
+} as const;
+
+export type FontSize = keyof typeof fontSizeMap;
+
+export const fontWeightMap = {
+  light: "font-light",
+  normal: "font-normal",
+  medium: "font-medium",
+  semibold: "font-semibold",
+  bold: "font-bold",
+  extrabold: "font-extrabold",
+  black: "font-black",
+} as const;
+
+export type FontWeight = keyof typeof fontWeightMap;
+
+export const letterSpacingMap = {
+  tighter: "tracking-tighter",
+  tight: "tracking-tight",
+  normal: "tracking-normal",
+  wide: "tracking-wide",
+  wider: "tracking-wider",
+  widest: "tracking-widest",
+} as const;
+
+export type LetterSpacing = keyof typeof letterSpacingMap;
