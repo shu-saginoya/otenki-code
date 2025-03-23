f90957231b74469534489b3a3679e3eddba4bb74
shu.saginoya <shu.saginoya@gmail.com>
2025-03-24 01:01:27 +0900

    Update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ export default {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/styles/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/utils/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
