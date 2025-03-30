9c426fecc07733c0ba64a5102724e2741a7e698c
shu.saginoya <shu.saginoya@gmail.com>
2025-03-30 19:32:12 +0900

    Update app/select-area

--- src/app/select-area/page.tsx
+++ src/app/select-area/page.tsx
@@ -1,49 +1,137 @@
 "use client";
 
-import { Grid, Col, Text, Card, List, ListItem, Button } from "@/components";
-import { useSelectArea } from "@/hooks/useSelectArea";
+import type { Area } from "@/types";
+import { useState } from "react";
+import {
+  Grid,
+  Col,
+  Text,
+  Card,
+  AreaOptionsList,
+  Carousel,
+  CarouselItem,
+} from "@/components";
+import { useSelectArea, useAppRouter } from "@/hooks";
+import { useDispatch } from "react-redux";
+import { setArea } from "@/lib/features/areas/areasSlice";
 
 export default function SelectArea() {
-  const { options, loading, error, setNewArea, removeNewArea } =
-    useSelectArea();
+  const {
+    newCenter,
+    newOffice,
+    newClass10,
+    setNewCenter,
+    setNewOffice,
+    setNewClass10,
+    removeNewCenter,
+    removeNewOffice,
+    centerOptions,
+    officeOptions,
+    class10Options,
+    loading,
+    error,
+  } = useSelectArea();
 
-  if (loading) return <Text>Loading...</Text>;
-  if (error) return <Text>Error: {error}</Text>;
+  const { navigateTo, goBack } = useAppRouter();
 
-  if (options)
+  const [focus, setFocus] = useState(0);
+
+  const dispatch = useDispatch();
+  // Redux の state 更新処理
+  const saveStore = (areaLv1: Area, areaLv2: Area, areaLv3: Area) => {
+    dispatch(
+      setArea({
+        areaLv1,
+        areaLv2,
+        areaLv3,
+      })
+    );
+  };
+
+  const selectNewCenter = (area: Area) => {
+    setNewCenter(area);
+    setFocus(1);
+  };
+
+  const selectNewOffice = (area: Area) => {
+    setNewOffice(area);
+    setFocus(2);
+  };
+
+  const finalizeSelection = (area: Area) => {
+    setNewClass10(area);
+    if (newCenter && newOffice && newClass10) {
+      saveStore(newCenter, newOffice, newClass10);
+      navigateTo("home");
+    }
+  };
+
+  const goBackCenter = () => {
+    removeNewCenter();
+    setFocus(0);
+  };
+
+  const goBackOffice = () => {
+    removeNewOffice();
+    setFocus(1);
+  };
+
+  if (loading)
+    return (
+      <div role="status" className="flex h-full animate-pulse flex-col gap-2">
+        <div className="mx-auto h-4 w-full max-w-[320px] rounded-full bg-background-light dark:bg-foreground-light"></div>
+        <div className="h-[800px] w-full rounded bg-background-light dark:bg-foreground-light"></div>
+        <span className="sr-only">Loading...</span>
+      </div>
+    );
+
+  if (error)
     return (
       <Grid>
         <Col cols={12} justify="center">
-          <Text>いちばん近い地域を選択してください</Text>
-        </Col>
-        <Col cols={12}>
           <Card>
-            <List>
-              {Object.entries(options).map(([key, value]) => (
-                <ListItem key={key}>
-                  <Button
-                    color="none"
-                    appendIcon="arrowForward"
-                    block
-                    onClick={() => setNewArea({ name: value.name, code: key })}
-                  >
-                    {value.name}
-                  </Button>
-                </ListItem>
-              ))}
-              <ListItem>
-                <Button
-                  color="none"
-                  appendIcon="arrowBackUp"
-                  block
-                  onClick={removeNewArea}
-                >
-                  もどる
-                </Button>
-              </ListItem>
-            </List>
+            <Text>Error: {error}</Text>
           </Card>
         </Col>
       </Grid>
     );
+
+  return (
+    <Grid>
+      <Col cols={12} justify="center">
+        <Text>いちばん近い地域を選択してください</Text>
+      </Col>
+      <Col cols={12}>
+        <Carousel focus={focus} watchDrag={false}>
+          <CarouselItem>
+            <Card>
+              <AreaOptionsList
+                options={centerOptions || {}}
+                action={selectNewCenter}
+                goBackAction={goBack}
+              />
+            </Card>
+          </CarouselItem>
+          <CarouselItem>
+            <Card>
+              <AreaOptionsList
+                options={officeOptions || {}}
+                action={selectNewOffice}
+                goBackAction={goBackCenter}
+              />
+            </Card>
+          </CarouselItem>
+          <CarouselItem>
+            <Card>
+              <AreaOptionsList
+                options={class10Options || {}}
+                action={finalizeSelection}
+                goBackAction={goBackOffice}
+              />
+            </Card>
+          </CarouselItem>
+        </Carousel>
+      </Col>
+    </Grid>
+  );
 }
