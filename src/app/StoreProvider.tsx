9c90878b4cd45f23c768a8523663a2a8dbd8d062
shu.saginoya <shu.saginoya@gmail.com>
2025-07-09 00:09:42 +0900

    Update StoreProvider

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -3,7 +3,7 @@
 import { useEffect, useRef } from "react";
 import { Provider } from "react-redux";
 
-import { initArea } from "@/lib/features/areas/areasSlice";
+import { initSelectedArea } from "@/lib/features/areas/areasSlice";
 import { makeStore } from "@/lib/store";
 
 export default function StoreProvider({
@@ -14,8 +14,8 @@ export default function StoreProvider({
   const storeRef = useRef(makeStore());
 
   useEffect(() => {
-    // Store の初期化時に一度だけ initArea を実行
-    storeRef.current.dispatch(initArea());
+    // Store の初期化時に一度だけ initSelectedArea を実行
+    storeRef.current.dispatch(initSelectedArea());
   }, []);
 
   return <Provider store={storeRef.current}>{children}</Provider>;
