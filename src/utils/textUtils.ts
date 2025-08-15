fe65f1274b4102f1861bb245d91cc65774985a93
shu.saginoya <shu.saginoya@gmail.com>
2025-08-15 23:26:01 +0900

    Update Text Utils add toHalfWidthSpace

--- src/utils/textUtils.ts
+++ src/utils/textUtils.ts
@@ -8,3 +8,12 @@ export const toHalfWidth = (str: string): string => {
     String.fromCharCode(ch.charCodeAt(0) - 0xfee0)
   );
 };
+
+/**
+ * 全角スペースを半角スペースに変換する
+ * @param str - 変換する文字列
+ * @returns 変換後の文字列
+ */
+export const toHalfWidthSpace = (str: string): string => {
+  return str.replace(/　/g, " ");
+};
