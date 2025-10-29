52f83125815170790533779bd702cf91bc557515
shu.saginoya <shu.saginoya@gmail.com>
2025-10-29 12:57:19 +0900

    Add data/clothingImages.

---
+++ src/data/clothingImages.ts
@@ -0,0 +1,144 @@
+import type { ClothingImageMap } from "@/types";
+
+export const clothingImages: ClothingImageMap = {
+  cap: {
+    path: "/images/clothing/cap.png",
+    name: "キャップ",
+    category: "outer",
+  },
+  cardigan: {
+    path: "/images/clothing/cardigan.png",
+    name: "カーディガン",
+    category: "outer",
+  },
+  downCoat: {
+    path: "/images/clothing/down-coat.png",
+    name: "ダウンコート",
+    category: "outer",
+  },
+  dressShirtShort: {
+    path: "/images/clothing/dress-shirt-short.png",
+    name: "半袖ワイシャツ",
+    category: "tops",
+  },
+  dressShirt: {
+    path: "/images/clothing/dress-shirt.png",
+    name: "ワイシャツ",
+    category: "tops",
+  },
+  duffleCoat: {
+    path: "/images/clothing/duffle-coat.png",
+    name: "ダッフルコート",
+    category: "outer",
+  },
+  furCoat: {
+    path: "/images/clothing/fur-coat.png",
+    name: "ファーコート",
+    category: "outer",
+  },
+  hat: {
+    path: "/images/clothing/hat.png",
+    name: "帽子",
+    category: "outer",
+  },
+  hoodie: {
+    path: "/images/clothing/hoodie.png",
+    name: "パーカー",
+    category: "tops",
+  },
+  jacket: {
+    path: "/images/clothing/jacket.png",
+    name: "ジャケット",
+    category: "outer",
+  },
+  overalls: {
+    path: "/images/clothing/overalls.png",
+    name: "オーバーオール",
+    category: "bottoms",
+  },
+  pantsLong: {
+    path: "/images/clothing/pants-long.png",
+    name: "長ズボン",
+    category: "bottoms",
+  },
+  pantsMiddle: {
+    path: "/images/clothing/pants-middle.png",
+    name: "ミドル丈パンツ",
+    category: "bottoms",
+  },
+  pantsShort: {
+    path: "/images/clothing/pants-short.png",
+    name: "短パン",
+    category: "bottoms",
+  },
+  peaCoat: {
+    path: "/images/clothing/pea-coat.png",
+    name: "ピーコート",
+    category: "outer",
+  },
+  skirtLong: {
+    path: "/images/clothing/skirt-long.png",
+    name: "ロングスカート",
+    category: "bottoms",
+  },
+  skirtMiddle: {
+    path: "/images/clothing/skirt-middle.png",
+    name: "ミドル丈スカート",
+    category: "bottoms",
+  },
+  skirtShort: {
+    path: "/images/clothing/skirt-short.png",
+    name: "ミニスカート",
+    category: "bottoms",
+  },
+  sweater: {
+    path: "/images/clothing/sweater.png",
+    name: "セーター",
+    category: "tops",
+  },
+  tShirtLong: {
+    path: "/images/clothing/T-shirt-long.png",
+    name: "長袖Tシャツ",
+    category: "tops",
+  },
+  tShirt: {
+    path: "/images/clothing/T-shirt.png",
+    name: "Tシャツ",
+    category: "tops",
+  },
+  tanktop: {
+    path: "/images/clothing/tanktop.png",
+    name: "タンクトップ",
+    category: "tops",
+  },
+  trenchCoat: {
+    path: "/images/clothing/trench-coat.png",
+    name: "トレンチコート",
+    category: "outer",
+  },
+  turtleneck: {
+    path: "/images/clothing/turtleneck.png",
+    name: "タートルネック",
+    category: "tops",
+  },
+  umbrella: {
+    path: "/images/clothing/umbrella.png",
+    name: "傘",
+    category: "outer",
+  },
+  varsityJacket: {
+    path: "/images/clothing/varsity-jacket.png",
+    name: "スタジャン",
+    category: "outer",
+  },
+  vest: {
+    path: "/images/clothing/vest.png",
+    name: "ベスト",
+    category: "tops",
+  },
+  windbreaker: {
+    path: "/images/clothing/windbreaker.png",
+    name: "ウインドブレーカー",
+    category: "outer",
+  },
+};
